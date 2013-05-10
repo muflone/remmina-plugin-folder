120d76945e28acf510f8fc37a6f3b94061971eba
Muflone <muflone@vbsimple.net>
2013-05-11 01:27:08 +0200

    First code commit

---
+++ remmina-plugin-folder/src/plugin_config.h
@@ -0,0 +1,8 @@
+#ifndef __PLUGIN_CONFIG_H
+#define __PLUGIN_CONFIG_H
+
+#define PLUGIN_NAME        "FOLDER"
+#define PLUGIN_DESCRIPTION "FOLDER - Open a folder"
+#define PLUGIN_VERSION     "1.0.0.0"
+#define PLUGIN_APPICON     "remmina-folder"
+#endif
