4e4db20ff729a37dfe04d757e0214663a352fb6c
Muflone <webreg@vbsimple.net>
2014-11-02 13:38:08 +0100

    Minor code cleanup

--- remmina-plugin-folder/src/plugin_config.h
+++ remmina-plugin-folder/src/plugin_config.h
@@ -1,8 +1,29 @@
-#ifndef __PLUGIN_CONFIG_H
-#define __PLUGIN_CONFIG_H
+/*
+ *     Project: Remmina Plugin FOLDER
+ * Description: Remmina protocol plugin to open a folder.
+ *      Author: Fabio Castelli (Muflone) <muflone@vbsimple.net>
+ *   Copyright: 2012-2014 Fabio Castelli (Muflone)
+ *     License: GPL-2+
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of ERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-#define PLUGIN_NAME        "FOLDER"
-#define PLUGIN_DESCRIPTION "FOLDER - Open a folder"
-#define PLUGIN_VERSION     "1.1.1.0"
-#define PLUGIN_APPICON     "remmina-folder"
+#ifndef __PLUGIN_CONFIG_H
+  #define __PLUGIN_CONFIG_H
+
+  #define PLUGIN_NAME        "FOLDER"
+  #define PLUGIN_DESCRIPTION "FOLDER - Open a folder"
+  #define PLUGIN_VERSION     "1.1.1.0"
+  #define PLUGIN_APPICON     "remmina-folder"
 #endif
