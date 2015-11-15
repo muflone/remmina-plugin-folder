771f794f266179e945ad72ff8e1adfe06c7f0fe1
Muflone <webreg@vbsimple.net>
2015-11-15 20:55:17 +0100

    Bumped version number

--- remmina-plugin-folder/src/plugin_config.h
+++ remmina-plugin-folder/src/plugin_config.h
@@ -24,6 +24,6 @@
 
   #define PLUGIN_NAME        "FOLDER"
   #define PLUGIN_DESCRIPTION "FOLDER - Open a folder"
-  #define PLUGIN_VERSION     "1.2.1.0"
+  #define PLUGIN_VERSION     "1.2.2.0"
   #define PLUGIN_APPICON     "remmina-folder"
 #endif
