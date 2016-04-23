7d9ede8eb1f1ee31944ef9862fe1d22a4ef36be0
Muflone <webreg@vbsimple.net>
2016-04-24 00:00:26 +0200

    Updated for Remmina API change and Builder 1.2.3.0

--- remmina-plugin-folder/src/plugin_config.h
+++ remmina-plugin-folder/src/plugin_config.h
@@ -24,6 +24,6 @@
 
   #define PLUGIN_NAME        "FOLDER"
   #define PLUGIN_DESCRIPTION "FOLDER - Open a folder"
-  #define PLUGIN_VERSION     "1.2.2.0"
+  #define PLUGIN_VERSION     "1.2.3.0"
   #define PLUGIN_APPICON     "remmina-folder"
 #endif
